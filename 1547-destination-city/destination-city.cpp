cea2849583387299c9943032cae30fccfa68d4e0
Kshitij  Patil <104363075+Kshitij722@users.noreply.github.com>
2023-12-15 09:40:38 +0530

    Time: 10 ms (62.92%) | Memory: 11.1 MB (59.81%) - LeetSync

--- 1547-destination-city/destination-city.cpp
+++ 1547-destination-city/destination-city.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
-        set<string> a;
         unordered_map<string,int> b;
         for(int i=0;i<paths.size();i++){
           b[paths[i][0]]++;
-          a.insert(paths[i][0]);
-          a.insert(paths[i][1]);
         }
-        string s1;
-        for(auto i:a){
-            if(b.find(i)==b.end()){
-                s1 = i;
-            }
+        for(int i=0;i<paths.size();i++){
+            if(b.find(paths[i][1]) == b.end()) return paths[i][1];
         }
-        return s1;
+        return "";
     }
 };
