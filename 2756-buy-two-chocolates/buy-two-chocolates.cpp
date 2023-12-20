f8c686fba1e63db7b34018cf3c3024fd63ac154f
Kshitij  Patil <104363075+Kshitij722@users.noreply.github.com>
2023-12-20 11:56:36 +0530

    Time: 25 ms (5.34%) | Memory: 46.9 MB (58.30%) - LeetSync

---
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(),prices.end());
+        int rem = money - (prices[0] + prices[1]);
+        if(rem<0) return money;
+        return rem;
+
+    }
+};
