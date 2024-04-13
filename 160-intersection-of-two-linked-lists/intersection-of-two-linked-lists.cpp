d42f77f3ab2fe4f639942af379bf8307446fbab8
Kshitij  Patil <104363075+Kshitij722@users.noreply.github.com>
2024-04-13 19:46:17 +0530

    Time: 43 ms (34.85%) | Memory: 17.1 MB (34.17%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,44 +9,26 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* dummy1 = headA;
-        ListNode* dummy2 = headB;
-        int c1=0,c2=0;
-        while(dummy1!=NULL || dummy2!=NULL){
-            if(dummy1!=NULL){
-                c1++;
-                dummy1=dummy1->next;
+        if(headA ==NULL || headB==NULL) return NULL;
+        ListNode* d1 = headA;
+        ListNode* d2 = headB;
+
+        while(d1!=d2){
+            if(d1!=NULL){
+                d1 = d1->next;
             }
-            if(dummy2!=NULL){
-                c2++;
-                dummy2=dummy2->next;
+            else{
+                d1 = headB;
             }
+            if(d2!=NULL){
+                d2 = d2->next;
+            }
+            else{
+                d2 = headA;
+            }
+
+          
         }
-        int n;
-        dummy1 = headA;
-        dummy2 = headB;
-        if(c1>c2){
-            n = c1-c2;
-            for(int i=0;i<n;i++){
-            dummy1=dummy1->next;
-        }
-        }
-        else{
-            n = c2-c1;
-            for(int i=0;i<n;i++){
-            dummy2=dummy2->next;
-        }
-        }
-    
-        while(dummy1!=NULL && dummy2!=NULL){
-             if(dummy1 == dummy2){
-                return dummy1;
-             }
-             else{
-                dummy1 = dummy1->next;
-                dummy2 = dummy2->next;
-             }
-        }
-        return NULL;
+        return d1;
     }
 };
