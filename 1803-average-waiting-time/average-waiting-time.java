4e79fa7c0abdcededd64e32da0e49542d4f519e7
Kshitij  Patil <104363075+Kshitij722@users.noreply.github.com>
2024-07-09 10:46:26 +0530

    Time: 3 ms (93.06%) | Memory: 72.6 MB (58.37%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.java
+++ 1803-average-waiting-time/average-waiting-time.java
@@ -1,20 +1,22 @@
 class Solution {
     public double averageWaitingTime(int[][] customers) {
         double total = 0;
-        int currTime = 0;
+        int currTime = customers[0][0];
         for(int i=0;i<customers.length;i++){
             
             if(customers[i][0] > currTime){
-                currTime = customers[i][0];
-            
+                currTime = customers[i][0] + customers[i][1];
+                int total_w = customers[i][1];
+                total += total_w;
             }
-            int total_w = currTime - customers[i][0] + customers[i][1];
+            else{
+            currTime += customers[i][1];
+            int total_w = currTime - customers[i][0];
             total += total_w;
-            currTime+=customers[i][1];
-            
+            }
         }
 
-       
+        
         return total/customers.length;
     }
 }
