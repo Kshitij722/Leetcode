69a024ed8cdb96a273feeca1b04f2fdd7a62faf2
Kshitij  Patil <104363075+Kshitij722@users.noreply.github.com>
2024-08-20 09:58:06 +0530

    Time: 2 ms (99.86%) | Memory: 44.2 MB (83.57%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    public static boolean isCycle(ArrayList<Integer>[] graph, int curr, boolean[] visited, boolean[] rec){
+        rec[curr] = true;
+        visited[curr] = true;
+        for(int i=0;i<graph[curr].size();i++){
+            int n = graph[curr].get(i);
+            if(rec[n]) return true;
+            else if(!visited[n] && isCycle(graph,n, visited, rec)){
+                return true;
+            }
+        }
+        rec[curr] = false;
+        return false;
+    }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        boolean visited[] = new boolean[numCourses];
+        boolean rec[] = new boolean[numCourses];
+        ArrayList[] graph = new ArrayList[numCourses];
+        for(int i=0;i<numCourses;i++)
+            graph[i] = new ArrayList();
+            
+        for(int i=0; i<prerequisites.length;i++){
+            // degree[prerequisites[i][1]]++;
+            graph[prerequisites[i][0]].add(prerequisites[i][1]);
+        }
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i]){
+                boolean cycle = isCycle(graph, i, visited, rec);
+                if(cycle) return false;
+            }
+        }
+        return true;
+    }
+}
