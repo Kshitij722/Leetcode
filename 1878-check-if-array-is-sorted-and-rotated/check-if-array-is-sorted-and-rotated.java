dbf8a1004a6e1a2acf91f0f7586b47336b78ab47
Kshitij  Patil <104363075+Kshitij722@users.noreply.github.com>
2026-08-16 10:24:38 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (47.49%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,13 +1,25 @@
 class Solution {
-    public boolean check(int[] nums) {
-        int count = 0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>nums[(i+1)%nums.length]){
-                count++;
+
+    private static boolean solve(int[] nums){
+        boolean isDipped = false;
+        for(int i=0; i<nums.length-1; i++){
+            if(nums[i] > nums[i+1]){
+                if(!isDipped){
+                    isDipped = true;
+                }
+                else{
+                    return false;
+                }
             }
-            if(count>1) return false;
         }
-        
+        if(nums[nums.length-1] > nums[0]){
+            if(isDipped) return false;
+        } 
+
         return true;
+    }
+
+    public boolean check(int[] nums) {
+        return solve(nums);
     }
 }
